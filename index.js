21cdb1c4fdd1fd386ad879b290bae37cc9c76d0d
Stephen Whitmore <sww@eight45.net>
2017-05-14 17:07:18 -0400

    store<->store replication beginnings

--- index.js
+++ index.js
@@ -19,9 +19,38 @@ MediaStore.prototype.createWriteStream = function (name) {
 }
 
 MediaStore.prototype.list = function (cb) {
-  fs.readdir(this._dir, cb)
+  fs.readdir(this._dir, function (err, files) {
+    if (err && err.code === 'ENOENT') cb(null, [])
+    else if (err) cb(err)
+    else cb(null, files)
+  })
 }
 
 MediaStore.prototype.replicateStore = function (otherStore) {
+  // TODO: handle errors
+  this.list(function (err, myNames) {
+    otherStore.list(function (err, yourNames) {
+      var myWant = missing(myNames, yourNames)
+      console.log('I want', myWant)
+
+      var yourWant = missing(yourNames, myNames)
+      console.log('you want', yourWant)
+    })
+  })
 }
 
+// What's in 'b' that is not in 'a'?
+// [t], [t] -> [t]
+function missing (a, b) {
+  var m = []
+  var amap = {}
+  a.forEach(function (v) { amap[a] = true })
+
+  b.forEach(function (v) {
+    if (!amap[v]) {
+      m.push(v)
+    }
+  })
+
+  return m
+}

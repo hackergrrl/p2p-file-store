5bf622c27e025b9152426cc3192666a2e7526923
Stephen Whitmore <sww@eight45.net>
2017-05-15 12:28:06 -0400

    more thorough sync test

--- test/test.js
+++ test/test.js
@@ -76,5 +76,73 @@ test('1 file <-> empty', function (t, dir, done) {
   }
 })
 
-// - replicateStore: 3 files <-> 2 files
-// - replicateStore: 1 file <-> 1 file (dedupe => no change)
+// - replicateStore: 3 files <-> 2 files (1 in common)
+test('3 files <-> 2 files (1 in common)', function (t, dir, done) {
+  var root1 = path.join(dir, '1')
+  var store1 = Store(root1)
+  var root2 = path.join(dir, '2')
+  var store2 = Store(root2)
+
+  var pending = 4
+  writeFile(store1, '2010-01-01_foo.png', 'hello', written)
+  writeFile(store1, '2010-02-05_bar.png', 'goodbye', written)
+  writeFile(store1, '1976-12-17_quux.png', 'unix', written)
+  writeFile(store2, '1900-01-01_first.png', 'elder', written)
+  writeFile(store2, '2010-01-05_bar.png', 'goodbye', written)
+
+  function written (err) {
+    t.error(err)
+    if (--pending === 0) replicate()
+  }
+
+  function replicate () {
+    store2.replicateStore(store1, check)
+  }
+
+  function check (err) {
+    t.error(err)
+
+    // Four files in each store
+    t.equal(fs.readdirSync(root1).length, 4)
+    t.equal(fs.readdirSync(root2).length, 4)
+
+    // Two files in the 2010-01 subdir
+    t.equal(fs.readdirSync(path.join(root1, '2010-01')).length, 2)
+    t.equal(fs.readdirSync(path.join(root2, '2010-01')).length, 2)
+
+    // Check all files: store 1
+    t.ok(fs.existsSync(path.join(root1, '2010-01')))
+    t.equal(fs.readFileSync(path.join(root1, '2010-01', '2010-01-01_foo.png'), 'utf8'), 'hello')
+    t.ok(fs.existsSync(path.join(root1, '2010-01')))
+    t.equal(fs.readFileSync(path.join(root1, '2010-01', '2010-01-05_bar.png'), 'utf8'), 'goodbye')
+    t.ok(fs.existsSync(path.join(root1, '1976-12')))
+    t.equal(fs.readFileSync(path.join(root1, '1976-12', '1976-12-17_quux.png'), 'utf8'), 'unix')
+    t.ok(fs.existsSync(path.join(root1, '1976-12')))
+    t.equal(fs.readFileSync(path.join(root1, '1976-12', '1976-12-17_quux.png'), 'utf8'), 'unix')
+    t.ok(fs.existsSync(path.join(root1, '1900-01')))
+    t.equal(fs.readFileSync(path.join(root1, '1900-01', '1900-01-01_first.png'), 'utf8'), 'elder')
+
+    // Check all files: store 2
+    t.ok(fs.existsSync(path.join(root2, '2010-01')))
+    t.equal(fs.readFileSync(path.join(root2, '2010-01', '2010-01-01_foo.png'), 'utf8'), 'hello')
+    t.ok(fs.existsSync(path.join(root2, '2010-01')))
+    t.equal(fs.readFileSync(path.join(root2, '2010-01', '2010-01-05_bar.png'), 'utf8'), 'goodbye')
+    t.ok(fs.existsSync(path.join(root2, '1976-12')))
+    t.equal(fs.readFileSync(path.join(root2, '1976-12', '1976-12-17_quux.png'), 'utf8'), 'unix')
+    t.ok(fs.existsSync(path.join(root2, '1976-12')))
+    t.equal(fs.readFileSync(path.join(root2, '1976-12', '1976-12-17_quux.png'), 'utf8'), 'unix')
+    t.ok(fs.existsSync(path.join(root2, '1900-01')))
+    t.equal(fs.readFileSync(path.join(root2, '1900-01', '1900-01-01_first.png'), 'utf8'), 'elder')
+
+    done()
+    t.end()
+  }
+})
+
+function writeFile (store, name, data, done) {
+  var ws = store.createWriteStream(name)
+  ws.on('finish', done)
+  ws.on('error', done)
+  ws.write(data)
+  ws.end()
+}

c27f5c1e9218349ae7e12971bdfaf7722269c59b
Stephen Whitmore <sww@eight45.net>
2017-05-15 16:49:10 -0400

    add example src

---
+++ example.js
@@ -0,0 +1,25 @@
+var MediaStore = require('./')
+var fs = require('fs')
+
+var store1 = MediaStore('/tmp/media-one')
+var store2 = MediaStore('/tmp/media-two')
+
+var pending = 2
+
+var ws1 = store1.createWriteStream('foo_bar.png', doneWriting)
+ws1.end('hello')
+
+var ws2 = store2.createWriteStream('baz_bax.png', doneWriting)
+ws2.end('greetings')
+
+function doneWriting () {
+  if (--pending === 0) {
+    replicate()
+  }
+}
+
+function replicate () {
+  store1.replicateStore(store2, function (err) {
+    store2.createReadStream('foo_bar.png').pipe(process.stdout)
+  })
+}
